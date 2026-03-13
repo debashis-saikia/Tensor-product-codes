1e1b092574539a65069dd650fcbc1629c0a66d07
Debashis Saikia <saikiadebashis5@gmail.com>
2026-03-13 11:40:15 +0530

    Add files via upload

---
+++ Examples/[[20, 4]]/Circuit/[[20, 4]]_qiskit_circ.py
@@ -0,0 +1,6 @@
+from qiskit import QuantumCircuit
+
+circ = f'C:\\Users\\IISER13\\OneDrive\\Desktop\\Pavel Panteleev\\Tensor Product Codes\\[20, 4]_syndrome.qasm'
+
+qc = QuantumCircuit.from_qasm_file(circ)
+qc.draw('mpl', fold = 1,filename='[20, 4]_syndrome.png')
